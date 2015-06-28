0cdf7d68f01e06e08529ded089dbe473e101103b
yunux <c.yonas@gmail.com>
2015-06-28 22:20:58 +0100

    changed Thai.py to correct unicode conversion

--- cgi-bin/Thai.py
+++ cgi-bin/Thai.py
@@ -3,96 +3,300 @@
 """
 Created on Wed Apr 22 11:31:22 2015
 
-@author: jbfiot
+@author:    Jonas C. Villumsen
+@version:   June 2015
 """
 
-import re   # for parsing regular expressions
 
-FILE = '/home/jbfiot/www/cgi-bin/data/thai.txt'
+#==============================================================================
+# MODULES
+#==============================================================================
+
+# Encoding
+#import locale                                  # Ensures that subsequent open()s 
+#locale.getpreferredencoding = lambda: 'UTF-8'  # are UTF-8 encoded.
+#utf8stdout = open(1, 'w', encoding='utf-8', closefd=False) # fd 1 is stdout
+#from functools import partial
+#utf8print = partial(print, end='\r\n', file=utf8stdout)
+#utf8print = partial(print, end='\n', file=utf8stdout)
+import codecs
+
+# CGI debugging
+import cgitb
+cgitb.enable()
+DEBUG = 0
+
+# AWA components
+from Block import Block
+import UI
+
+# Web requests
+#import urllib.request
+#import urllib2
+
+import sys
+
+# Method 
+METHOD = 'dic' # available methods: 'dic', 'Naver' (to be implemented)
+
+# Dictionnary
+FILE = '../data/thai.txt'
 
 
+# unicode conversion
+if sys.version_info.major == 2:
+    # guarantee unicode string
+    _u = lambda t: t.decode('UTF-8', 'replace') if isinstance(t, str) else t
+    _uu = lambda *tt: tuple(_u(t) for t in tt)
+    # guarantee byte string in UTF8 encoding
+    _u8 = lambda t: t.encode('UTF-8', 'replace') if isinstance(t, unicode) else t
+    _uu8 = lambda *tt: tuple(_u8(t) for t in tt)
+else:
+    # guarantee unicode string
+    _u = lambda t: t.decode('UTF-8', 'replace') if isinstance(t, bytes) else t
+    _uu = lambda *tt: tuple(_u(t) for t in tt)
+    # guarantee byte string in UTF8 encoding
+    _u8 = lambda t: t.encode('UTF-8', 'replace') if isinstance(t, str) else t
+    _uu8 = lambda *tt: tuple(_u8(t) for t in tt)
+
+#==============================================================================
+# SYMBOL CLASSES
+#==============================================================================
 # consonants
-C = u'[ก-ฮ]'
+C = '[ก-ฮ]'
 # low class consonants
-LC = u'[งนมรนยญวล]'
+LC = '[งนมรนยญวล]'
 # consonant clusters
-CC = u'(กร|กล|กว|ขร|ขล|ขว|คร|คล|คว|ปร|ปล|ผล|พร|พล|ตร)'
+CC = '(กร|กล|กว|ขร|ขล|ขว|คร|คล|คว|ปร|ปล|ผล|พร|พล|ตร)'
 # all consonants
-AC = u'({} | {})'.format(C, CC)
+AC = '({c} | {cc})'.format(c=C, cc=CC)
+#AC = _u('({c} )'.format(c=C))
 # leading vowels
-F = u'[เ-ไ]' # 'เ,แ,ไ,ใ,โ'
+F = '[เ-ไ]' # 'เ,แ,ไ,ใ,โ'
 # trailing vowels
-R = u'[ะาำๅ]'  # ะ,า,◌ำ,
-R1 = u'[าำๅ]'  # า,◌ำ,ๅ
+R = '[ะาำๅ]'  # ะ,า,◌ำ,
+R1 = '[าำๅ]'  # า,◌ำ,ๅ
 # upper vowels
-U = u'[ัิีึื]' # ◌ั, ◌ิ, ◌ี, ◌ึ, ◌ื,◌
-U1 = u'[ิีึื]' # ◌ิ, ◌ี, ◌ึ, ◌ื,◌
+U = '[ัิีึื]' # ◌ั, ◌ิ, ◌ี, ◌ึ, ◌ื,◌
+U1 = '[ิีึื]' # ◌ิ, ◌ี, ◌ึ, ◌ื,◌
 # lower vowels
-L = u'[ุู]' # ◌ุ,◌ู,◌
+L = '[ุู]' # ◌ุ,◌ู,◌
 # tonal marks
-T = u'[่-๋]'
+T = '[่-๋]'
 # final consonants (not followed by tonal mark or vowel)
-Z = u'[กขฃคฅฆงจชซญฎฏฐฑฒณดตถทธนบปพฟภมรฤลฦศษสฬวย](?!{t}|{u}|{l}|{r})'.format(t=T, u=U, l=L,r=R)
+Z = '[กขฃคฅฆงจชซญฎฏฐฑฒณดตถทธนบปพฟภมรฤลฦศษสฬวย](?!{t}|{u}|{l}|{r})'.format(t=T, u=U, l=L,r=R)
 # silent syllables
-S = u'{c}({u}|{l})?{t}?์{{1}}'.format(c=C,u=U,l=L,t=T)
+S = '{c}({u}|{l})?{t}?์{{1}}'.format(c=C,u=U,l=L,t=T)
 
-suffixes = {}
 
-def get_meaning(word):
-    f = open(FILE, 'r', encoding='utf-8')
-    lines = f.readlines()
-    f.close()
-    for line in lines:
-        if line.split(',')[0].strip() == word:
-            return line.split(',')[1].strip()
-    return ''
+
+#==============================================================================
+# LANGUAGE METHODS
+#==============================================================================
 
 def get_words_with_block(block, exclude=None):
-    f = open(FILE, 'r', encoding='utf-8')
-    lines = f.readlines()
-    f.close()
-    words = {}
-    for line in lines:
-        if block in get_blocks(line.split(',')[0]):
-            word = line.split(',')[0].strip()
-            if word != exclude:
-                words[word] = line.split(',')[1].strip()
-    return words
+    """
+    This functions returns a list of Thai words that rely on the same block.
+    """
     
+    # DICTIONNARY IMPLEMENTATION
+    if METHOD == 'dic':
+        f = codecs.open(FILE, 'r', encoding='utf-8')
+        lines = f.readlines()
+        f.close()
+        words = []
+        for line in lines:
+            # Implementation 1: using hangul syllables
+            if _u(block.get_string()) in line.split(',')[0]:
+                word = line.split(',')[0].strip()
+                if word != exclude:
+                    words.append(ThaiWord(string=word, \
+                                            meaning=line.split(',')[1].strip()))
 
-def get_blocks(txt):
+        return words
+
+def compute_block_meanings(blocks):
+    # DICTIONNARY IMPLEMENTATION
+    if METHOD == 'dic':
+        f = codecs.open(FILE, 'r', encoding='utf-8')
+        lines = f.readlines()
+        f.close()
+        for line in lines:
+            for block in blocks:
+                if _u(block.get_string()) == line.split(',')[0]:
+                    meaning = line.split(',')[1].strip()
+                    block.meaning = meaning
+        return blocks
+
+def compute_blocks_re(txt):
+    """Compute blocks for word based on regular expressions
+       Generates a dictionary of (block, number) pairs,
+       where block is the string representation of the block
+       and number is the number of times this block occurs in
+       txt.
+       Returns only the blocks."""
+    import re
+
+    METHOD = 'syllable'
+
     blocks = {}
 
-    #first vowel, single consonant
-    r1 = u'([เ-ไ][ก-อ][่-๋]?)'  
-    #first vowel, initial + final cons.
-    r2 = u'([เ-โ][ก-อ][่-๋]?[ก-อ])'
-    # consonants incl. initial silent consonants 
-    c1 = u'({cc}|ห{lc}|อย|{c})'.format(lc=LC, c=C, cc=CC)
-    # initial consonant + vowel (upper/lower/trailing/inherent) + final consonant
-    r3 = u'({c1}{c}?({c}{{0}}|({l}|{u})?{t}?|{t}?{r1}|{t}?อ){z}({s})?)'.format(c=C, c1=c1,u=U,l=L,r1=R1,r=R,t=T,z=Z,s=S)
-    # initial consonant + vowel (upper/lower/trailing) 
-    r4 = u'({c}({t}?อ|({l}|{u}){t}?|{t}?{r})์?)'.format(c=c1, u=U1, l=L, r=R, t=T)
-    # leading vowel
-    r5 = u'({f}{c}(็?{t}?{z}|{t}?{r}?|{u}?{t}?{z}))'.format(f=F,c=c1,t=T,r=R,u=U,z=Z,l=L)
-    # sara ia and sara uea
-    r6 = u'เ{c}(ี{t}?ย|ื{t}?อ)({z}|ว)?'.format(c=c1,t=T,z=Z)
-    # er, o, ao
-    r7 = u'({f}{c}(อ|า)ะ?)'.format(f=F,c=c1,t=T,r=R,u=U,z=Z)
-    ## double r
-    r8 = u'({c}รร{z}?)'.format(f=F,c=c1,t=T,r=R,u=U,z=Z)
+    if METHOD == 'word':
+        regexp = _u('บัตร|ประ|จำ|ตัว')
+    elif METHOD == 'syllable':
+        # consonants incl. initial silent consonants 
+        c1 = '({cc}|ห{lc}|อย|{c})'.format(lc=LC, c=C, cc=CC)
+        # initial consonant + vowel (upper/lower/trailing/inherent) + final consonant
+        r3 = _u('({c1}{c}?({c}{{0}}|({l}|{u})?{t}?|{t}?{r}|{t}?อ){z}({s})?)'.format(c=C, c1=c1,u=U,l=L,r=R1,t=T,z=Z,s=S))
+        # initial consonant + vowel (upper/lower/trailing) 
+        r4 = _u('({c}({t}?อ|({l}|{u}){t}?|{t}?{r})์?)'.format(c=c1, u=U1, l=L, r=R, t=T))
+        # leading vowel
+        r5 = _u('({f}{c}(็?{t}?{z}|{t}?{r}?|{u}?{t}?{z}))'.format(f=F,c=c1,t=T,r=R,u=U,z=Z))
+        # sara ia and sara uea
+        r6 = _u('เ{c}(ี{t}?ย|ื{t}?อ)({z}|ว)?'.format(c=c1,t=T,z=Z))
+        # er, o, ao
+        r7 = _u('({}{}(อ|า)ะ?)'.format(F,c1))
+        ## double r
+        r8 = _u('({c}รร{z}?)'.format(c=c1,z=Z))
+        regexp = _u('('+ r8 + '|' + r7 + '|' + r3 + '|' + r6 + '|' + r4 + '|' + r5 + ')')
 
-    regexp = u'('+ r8 + '|' + r7 + '|' + r3 + '|' + r6 + '|' + r4 + '|' + r5 + ')'
-    
     regex = re.compile(regexp)
-    patterns = regex.finditer(txt)
+    patterns = regex.finditer(_u(txt))
 
     for i in patterns:
-        block = i.group()#.decode('utf-8')
+
+        block = Block(_u(i.group()))
         if block in blocks:
             blocks[block] += 1
         else:
             blocks[block] = 1
 
+    # compute block meanings
+    blocks = compute_block_meanings(blocks)
 
-    return blocks
+    return blocks.keys()
+
+    
+
+class ThaiWord(object):
+    """ This class is used to manipulate Thai words. """
+    def __init__(self, string='', meaning=None, compute_ethym=False):
+        self.string = string  # e.g. user input string
+        self.language = 'Thai'
+        self.blocks = self.compute_blocks(compute_ethym)
+        self.selected_meaning = 0 # index of the selected meaning
+        self.meanings = self.compute_meanings() # Different meanings in English
+        
+        
+    #==========================================================================
+    #  GETTERS
+    #==========================================================================
+
+    def get_string(self):
+        """ String getter """
+        return self.string
+
+    def get_language(self):
+        """ Language getter """
+        return self.language
+
+    def get_blocks(self):
+        """ Blocks getter """
+        return self.blocks
+
+    def get_meaning(self):
+        """ Meaning getter """
+        #UI.render_info(self.meanings[self.selected_meaning])
+        return self.meanings[self.selected_meaning]
+
+    def get_blocks_for_selected_meaning(self):
+        """ Getter for the blocks corresponding to the selected meaning """
+        return self.blocks[self.selected_meaning]
+
+    def get_ethym(self):
+        return ''.join([block.get_ethym() for block in \
+                    self.blocks[self.selected_meaning] if block.get_ethym()])
+
+    #==========================================================================
+    #  PRINT METHODS
+    #==========================================================================
+
+    def print_blocks_for_selected_meaning(self):
+        """ This methods prints the block strings for the selected meaning. 
+        
+        Example:
+        --------
+            For the word '안녕', the blocks will be ['안', '녕']
+        """
+        UI.render_info('print blocks for selected meaning')
+        UI.render_info([block.get_str() for block in self.blocks[self.selected_meaning]])
+        return [block.get_str() for block in self.blocks[self.selected_meaning]]
+        
+
+    #==========================================================================
+    #   LANGUAGE METHODS        
+    #==========================================================================
+        
+    def compute_meanings(self):
+        """ Find the meaning.
+        Note:         
+        """
+        f = codecs.open(FILE, 'r', encoding='utf-8')
+        lines = f.readlines()
+        f.close()
+        for line in lines:
+            
+            if line.split(',')[0] == self.string:
+                #UI.render_info(line.split(',')[0])
+                return [line.split(',')[1].strip()]
+        return ['']
+        
+
+    def compute_blocks(self, compute_ethym=False):
+        """ Compute the blocks given the input string.
+
+        Output:
+            Returns a list of lists of blocks, i.e. 
+            [ [b11, ..., b1n1], [b21, ..., b2n2], ...], where each list of 
+            blocks [bi1, ..., bini] corresponds to a possible meaning of the 
+            input string.
+
+        Note: 
+            In this CSV dictionnary based implemenation, only one meaning is 
+            available.            
+        """
+        if DEBUG:
+            UI.render_info('compute_blocks() called for word ' + self.string)        
+        
+        suffixes = {'하다':'하다 verb particule', \
+            '합니다': 'formal 하다 ending', \
+            '하세요': 'formal imperative form of 하다', \
+              '요': 'politeness particle'}
+              
+        detected_suffix = ''
+        # for suffix in suffixes.keys():
+        #     if self.string.endswith(suffix):
+        #         detected_suffix = suffix
+        #         continue
+        body = self.string[0:len(self.string)-len(detected_suffix)]
+        #if not compute_ethym:
+            #blocks = [Block(body[i]) for i in range(len(body)) if body[i] != ' ']
+        blocks = compute_blocks_re(body)
+        # else:
+        #     hanja = get_hanja(body)
+        #     if DEBUG:
+        #         UI.render_info(hanja)
+        #     blocks = [Block(body[i], ethym=hanja[i]) for i in range(len(body)) if body[i] != ' ']
+
+        # if detected_suffix:
+        #     suffix_desc = 'Suffix: ' + suffixes[detected_suffix]
+        #     blocks.append(Block(detected_suffix, ethym=suffix_desc))
+
+        if DEBUG:
+            for block in blocks:
+                UI.render_info(block.get_string())
+
+        return [blocks]
+        
+
+

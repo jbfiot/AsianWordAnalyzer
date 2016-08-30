05d27e74413806a772ae0517e56e1864ac91a8dd
Jean-Baptiste Fiot (장밥) <jean-baptiste.fiot@centraliens.net>
2016-08-30 23:17:25 +0100

    Add DbUtil class and mofidy get_words_with_block to return a dataframe

--- asian_word_analyzer/korean/db.py
+++ asian_word_analyzer/korean/db.py
@@ -1,72 +1,68 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-from sqlalchemy import create_engine
+import sqlite3
+from functools import partialmethod
 
-from asian_word_analyzer.korean.word import KoreanWord
-import asian_word_analyzer.login as login
-from asian_word_analyzer.UI import render_info
+from asian_word_analyzer.login import connection_string
 from asian_word_analyzer.tools import detect_language
 
-DEBUG = False
+
+class DbUtil:
+    def __init__(self):
+        self.connection = sqlite3.connect(connection_string)        
+        
+    def compute_meanings(self, word):
+        """ Find the possible meanings based on the input string by the user"""
+        query = "SELECT meaning from Korean WHERE word='{}'".format(word)
+        df = pd.read_sql(query, self.connection)
+        if not df.empty:
+            return df.meaning
+        else:
+            return ['']
 
 
-def get_words_with_block(block, exclude=None):
-    """
-    This functions returns a list of Korean words which also contains blocks
-    with the same ethymology.
+    def get_hanja_x(self, hanja, x):
+        query = "SELECT {} from Korean_ethym WHERE ethym='{}'".format(x, hanja)
+        df = pd.read_sql(query, self.connection)
+        if not df.empty:
+            return df[x][0]
+        else:
+            return None
 
-    When the ethymology is not available, an empty list is returned. This
-    typically happens when the input block is a suffix.
-    """
-    if block.get_ethym():
-        query = """SELECT * FROM `Korean` WHERE INSTR( ethym, '""" + \
-                                            block.get_ethym() + """') >0"""
-        engine = create_engine(login.connection_string, echo=False)
-        results = pd.io.sql.execute(query, engine)
-        results = results.fetchall()
-        words = [KoreanWord(string=r[0], ethym=r[1], meaning=r[2]) \
-                    for r in results if r[0] != exclude and \
-                    len(r[0]) == len(r[1]) and \
-                    detect_language(r[1]) is not 'korean']
-                    # len check and ethym check to avoid some corrupted
-                    # data from the database to be displayed
-    else:
-        words = []
+    get_hanja_meaning = partialmethod(get_hanja_x, x='meaning')
 
-    return words
+    get_hanja_name = partialmethod(get_hanja_x, x='name')
 
-def get_hanja_meaning(hanja):
-    """ Get the meaning of a hanja character from the database """
-    if DEBUG:
-        render_info('get_hanja_meaning(...) called with hanja=' + hanja)
-    query = "SELECT meaning from Korean_ethym WHERE ethym='" + hanja + "'"
-    engine = create_engine(login.connection_string, echo=False)
-    results = pd.io.sql.execute(query, engine)
-    results = results.fetchall()
-    if results:
-        return results[0][0]
-    else:
-        return None
 
-def get_hanja_name(hanja):
-    """ Get the name of a hanja character from the database """
-    query = "SELECT name from Korean_ethym WHERE ethym='" + hanja + "'"
-    engine = create_engine(login.connection_string, echo=False)
-    results = pd.io.sql.execute(query, engine)
-    results = results.fetchall()
-    if results:
-        return results[0][0]
-    else:
-        return None
+    def get_words_with_block(self, block, exclude=None):
+        """
+        This functions returns a list of Korean words which also contains blocks
+        with the same ethymology.
+    
+        When the ethymology is not available, an empty list is returned. This
+        typically happens when the input block is a suffix.
+        """
+        if block.ethym:
+            query = """SELECT * FROM `Korean` WHERE ethym LIKE '%{}%'""".format(block.ethym)           
+            df = pd.read_sql(query, self.connection)
+            # TODO: add the exclude filter (+ filters for corrupted rows)
+            return df
 
-def compute_meanings(word):
-    """ Find the possible meanings based on the input string by the user"""
-    query = "SELECT meaning from Korean WHERE word='{}'".format(word)
-    engine = create_engine(login.connection_string, echo=False)
-    results = pd.io.sql.execute(query, engine)
-    results = results.fetchall()
-    if results:
-        return results[0]
-    else:
-        return ['']
+#            results = pd.io.sql.execute(query, self.connection)
+#            results = results.fetchall()
+#            words = [KoreanWord(string=r[0], ethym=r[1], meaning=r[2]) \
+#                        for r in results if r[0] != exclude and \
+#                        len(r[0]) == len(r[1]) and \
+#                        detect_language(r[1]) is not 'korean']
+#                        # len check and ethym check to avoid some corrupted
+#                        # data from the database to be displayed
+        else:
+            words = []
+    
+        return words
+
+
+
+
+

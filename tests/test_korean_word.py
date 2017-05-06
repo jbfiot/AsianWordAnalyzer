d993fc77f650823db27d706a872c1e118eb822b6
Jean-Baptiste Fiot (장밥) <jean-baptiste.fiot@centraliens.net>
2017-05-06 20:29:26 +0100

    Add test_init

--- tests/test_korean_word.py
+++ tests/test_korean_word.py
@@ -11,10 +11,17 @@ import pytest
 from asian_word_analyzer.korean.word import KoreanWord
 
 
-@pytest.mark.parametrize('word, expected', ((u'공부하다', u'하다'),
-#                                            (u'안녕하세요', u'하세요'),
-                                            (u'선생님', u'님')))
-def test_compute_suffix(word, expected):
-   assert expected == KoreanWord(word).suffix
-   
-   
+class TestKoreanWord:
+    
+    def test_init(self):
+        KoreanWord(u'안녕', u'安寧')
+    
+    def test_init_incorrect_ethym(self):
+        with pytest.raises(ValueError):
+            KoreanWord(u'안녕', ethym=u'安寧安寧')
+
+    @pytest.mark.parametrize('word, expected', ((u'공부하다', u'하다'),
+                                                # (u'안녕하세요', u'하세요'),
+                                                (u'선생님', u'님')))
+    def test_compute_suffix(self, word, expected):
+        assert expected == KoreanWord(word).suffix

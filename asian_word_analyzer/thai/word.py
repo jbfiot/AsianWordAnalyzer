8653eb5993a6807af5f23f4ba0d2828dddbd22d0
Jean-Baptiste Fiot (장밥) <jean-baptiste.fiot@centraliens.net>
2017-05-06 21:54:43 +0100

    Remove unused method and simplify code

--- asian_word_analyzer/thai/word.py
+++ asian_word_analyzer/thai/word.py
@@ -10,6 +10,7 @@ Created on Wed Apr 22 11:31:22 2015
 
 import cgitb
 import re
+from collections import defaultdict
 
 from asian_word_analyzer.block import Block
 import asian_word_analyzer.ui as ui
@@ -26,40 +27,23 @@ class ThaiWord(AsianWord):
     def __init__(self, string='', meaning=None, compute_ethym=False):
         self.string = string  # e.g. user input string
         self.language = 'Thai'
-        self.blocks = [self.compute_blocks_re(self.string)]
+        self.blocks = [self.compute_blocks(self.string)]
         self.selected_meaning = 0  # index of the selected meaning
         self.meanings = DbUtil().compute_meanings(string)  # Different meanings in English
         ui.render_info(self.meanings.head())
 
-    def compute_blocks(self, compute_ethym=False):
-        """ Compute the blocks given the input string.
-
-        Output:
-            Returns a list of lists of blocks, i.e. 
-            [ [b11, ..., b1n1], [b21, ..., b2n2], ...], where each list of 
-            blocks [bi1, ..., bini] corresponds to a possible meaning of the 
-            input string.
-
-        Note: 
-            In this CSV dictionary based implementation, only one meaning is 
-            available.            
-        """
-              
-        detected_suffix = ''
-        body = self.string[0:len(self.string)-len(detected_suffix)]
-        blocks = self.compute_blocks_re(body)
-        return [blocks]
-
     @staticmethod
-    def compute_blocks_re(txt, method='syllable'):
-        """Compute blocks for word based on regular expressions
-           Generates a dictionary of (block, number) pairs,
-           where block is the string representation of the block
-           and number is the number of times this block occurs in
-           txt.
-           Returns only the blocks."""
+    def compute_blocks(txt, method='syllable'):
+        """
+        Compute blocks for word based on regular expressions
         
-        blocks = {}
+        Generates a dictionary of (block, number) pairs, where block is the string representation of 
+        the block and number is the number of times this block occurs in txt.
+        
+        Returns only the blocks.
+        """
+        
+        blocks = defaultdict(int)
     
         if method == 'word':
             regexp = _u('บัตร|ประ|จำ|ตัว')
@@ -74,11 +58,8 @@ class ThaiWord(AsianWord):
         for i in patterns:
     
             block = Block(_u(i.group()))
-            if block in blocks:
-                blocks[block] += 1
-            else:
-                blocks[block] = 1
-    
+            blocks[block] += 1
+
         # compute block meanings
         blocks = DbUtil().compute_block_meanings(blocks)
     

de9c7d8e646bd27f5edc7eccaac0f50068fac326
Jean-Baptiste Fiot (장밥) <jean-baptiste.fiot@centraliens.net>
2017-05-06 20:31:07 +0100

    (fix pep8 warnings)

--- tests/test_utf8.py
+++ tests/test_utf8.py
@@ -6,6 +6,7 @@ import pytest
 
 from asian_word_analyzer.utf8 import _u, _u8
 
+
 @pytest.mark.skipif(sys.version_info.major < 3, reason="requires python>=3")
 @pytest.mark.parametrize('input_string', (b'a', 'a'))
 def test__u(input_string):
@@ -15,6 +16,4 @@ def test__u(input_string):
 @pytest.mark.skipif(sys.version_info.major < 3, reason="requires python>=3")
 @pytest.mark.parametrize('input_string', (b'a', 'a'))
 def test__u8(input_string):
-    assert isinstance(_u8(input_string), bytes)    
-    
-    
+    assert isinstance(_u8(input_string), bytes)

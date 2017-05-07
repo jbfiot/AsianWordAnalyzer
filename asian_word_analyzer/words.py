2165cc9fc1db40eddabeec7ceee6f39ad9961388
Jean-Baptiste Fiot (장밥) <jean-baptiste.fiot@centraliens.net>
2017-05-07 09:48:10 +0100

    Remove unused print_blocks_for_selected_meaning

--- asian_word_analyzer/words.py
+++ asian_word_analyzer/words.py
@@ -15,12 +15,3 @@ class AsianWord(object):
     def ethym(self):
         return ''.join([block.ethym for block in self.blocks[self.selected_meaning] if block.ethym])
 
-    def print_blocks_for_selected_meaning(self):
-        """ This methods prints the block strings for the selected meaning.
-
-        Example:
-        --------
-            For the word '안녕', the printed blocks will be ['안', '녕']
-        """
-        return [block.string for block in self.blocks[self.selected_meaning]]
-
